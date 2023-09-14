874022fd3f921de5b261566adac9ab9efd05f803
striu21 <134447678+striu21@users.noreply.github.com>
2023-09-14 18:51:01 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
-import launch
+import fall
 import os
 
 req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
@@ -6,5 +6,5 @@ req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requiremen
 with open(req_file) as file:
     for lib in file:
         lib = lib.strip()
-        if not launch.is_installed(lib):
-            launch.run_pip(f"install {lib}", f"Deforum requirement: {lib}")
+        if not fall.is_installed(lib):
+            fall.run_pip(f"install {lib}", f"Deforum requirement: {lib}")
